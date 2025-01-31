8bff546ee81036e7a8afcf1a92924a6691f61fcb
iisyos <kosuke1216a@yahoo.co.jp>
2025-02-01 02:35:45 +0900

    *chore: web camera 表示まで

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useRef } from "react";
 import "./App.css";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user",
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isCapturing, setIsCapturing] = useState(false);
+  const webcamRef = useRef<Webcam>(null);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <header className="App-header">
+        <button onClick={() => setIsCapturing(!isCapturing)}>
+          {isCapturing ? "Stop capturing" : "Start capturing"}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {isCapturing && (
+          <Webcam
+            audio={false}
+            height={720}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+            videoConstraints={videoConstraints}
+          />
+        )}
+      </header>
     </>
   );
 }
